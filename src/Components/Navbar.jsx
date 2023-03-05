abd1090e693f00f2ed9c1f2e0f3852af75f2e386
Godswill Adigwe <gcadigwe@gmail.com>
2023-03-05 21:35:28 +0100

    wallet connection

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { Wallet } from './WalletAdapter'
+import React from "react";
+import { Wallet } from "./WalletAdapter";
+import { useWallet } from "@solana/wallet-adapter-react";
 
 export default function Navbar() {
+  const { publicKey, wallet, disconnect } = useWallet();
+
+  console.log("wallet", wallet, publicKey);
   return (
     <nav className='navbar'>
-    <h1 className='hero-text'>The Burn List</h1>
-    <div className='wallet-container'>
-      <Wallet/>
-    </div>
-  </nav>
-  )
+      <h1 className='hero-text'>The Burn List</h1>
+      <div className='wallet-container'>
+        <Wallet />
+      </div>
+    </nav>
+  );
 }
