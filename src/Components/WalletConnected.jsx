70a557d47c50674fec11f3e7700eeb91d9ed6446
mankeylol <mankeyooooo@gmail.com>
2023-03-09 22:46:55 +0530

    added metaplex

--- src/Components/WalletConnected.jsx
+++ src/Components/WalletConnected.jsx
@@ -1,12 +1,18 @@
 import { useWallet } from "@solana/wallet-adapter-react";
+import { Metaplex } from "@metaplex-foundation/js";
+import { Connection, clusterApiUrl,} from "@solana/web3.js";
+
 import Card from './Card';
 import data from '../data'
 
 
 
+
 export default function WalletConnected() {
 
-  const { connected } = useWallet();
+  const { connected, publicKey } = useWallet();
+
+
 
   const cards = data.map(item =>{
     return (<Card 
@@ -18,7 +24,7 @@ export default function WalletConnected() {
   const MyModal = () =>{
     return(
       <div className='modal-content' >
-        <h2 classname ="content-heading">Possible Scam NFTs</h2>
+        <h2 className ="content-heading">Possible Scam NFTs</h2>
         <div className="nft-container">
         {cards}
         </div>
