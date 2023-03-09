373c9a3d9c4c556b98c64b8a40d6e15967e715e9
mankeylol <mankeyooooo@gmail.com>
2023-03-09 19:03:00 +0530

    added hello moon api

--- src/Components/WalletAdapter.jsx
+++ src/Components/WalletAdapter.jsx
@@ -6,7 +6,6 @@ import {
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
 import {
-  WalletModalProvider,
   WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
@@ -19,7 +18,7 @@ export const Wallet = () => {
   const network = WalletAdapterNetwork.Devnet;
 
   // You can also provide a custom RPC endpoint.
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
 
   console.log(network);
 
--- src/Components/WalletConnected.jsx
+++ src/Components/WalletConnected.jsx
@@ -1,12 +1,29 @@
-import { useWallet } from "@solana/wallet-adapter-react";
+import { useWallet, } from "@solana/wallet-adapter-react";
 import Card from './Card';
 import data from '../data'
 
 
 
+
+
 export default function WalletConnected() {
 
-  const { connected } = useWallet();
+  const { connected, publicKey } = useWallet();
+
+const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      authorization: 'Bearer f9322af6-68dd-4cce-a894-76bf23a0a005'
+    },
+    body: JSON.stringify({ownerAccount: publicKey })
+  };
+  
+  fetch('https://rest-api.hellomoon.io/v0/nft/mints-by-owner', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
 
   const cards = data.map(item =>{
     return (<Card 
@@ -18,11 +35,11 @@ export default function WalletConnected() {
   const MyModal = () =>{
     return(
       <div className='modal-content' >
-        <h2 classname ="content-heading">Possible Scam NFTs</h2>
+        <h2 className ="content-heading">Possible Scam NFTs</h2>
         <div className="nft-container">
         {cards}
         </div>
-        <button className="burn-btn">Burn and claim rent</button>
+        <button className="burn-btn" onClick={options}>Burn and claim rent</button>
       </div>
     )
   }
